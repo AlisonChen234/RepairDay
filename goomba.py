6a64ae07b66a68b30bd9d694f4185bbad732153f
AlisonChen234 <168639554+AlisonChen234@users.noreply.github.com>
2024-05-07 08:42:39 -0400

    Add files via upload

---
+++ goomba.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+class Goomba:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("goomba_sprite.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = .4
+        self.current_direction = "right"
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .5, self.image_size[1] * .5)
+        self.image = pygame.transform.scale(self.image, scale_size)
+        self.image_size = self.image.get_size()
+    def set_location(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def move_direction(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "left":
+            self.x = self.x - self.delta
+        if direction == "up":
+            self.y = self.y - self.delta
+        if direction == "down":
+            self.y = self.y + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from goomba import Goomba
 
 # set up pygame modules
 pygame.init()
@@ -11,26 +12,64 @@ size = (400, 300)
 screen = pygame.display.set_mode(size)
 
 
-welcome="Hi,welcome to my game! "
+welcome="Hi,welcome to my game!"
+welcome_1="Press the mouse to start the game!"
+welcome_2="Your objective here is to earn coins and use these coins to buy tools and help rebuild the city after a natural disaster."
 
 # render the text for later
 display_welcome = my_font.render(welcome, True, (255, 255, 255))
+display_welcome_1 = my_font.render(welcome_1, True, (255, 255, 255))
+display_welcome_2 = my_font.render(welcome_2, True, (255, 255, 255))
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
-
+start_screen= True
 # -------- Main Program Loop -----------
 while run:
+
+
+    keys = pygame.key.get_pressed()  # checking pressed keys
+    if keys[pygame.K_d]:
+        #g.move_direction("right")
+    if keys[pygame.K_w]:
+        #g.move_direction("up")
+    if keys[pygame.K_a]:
+        #g.move_direction("left")
+    if keys[pygame.K_s]:
+        #g.move_direction("down")
+
+    if keys[pygame.K_RIGHT]:
+    # g.move_direction("right")
+    if keys[pygame.K_UP]:
+    # g.move_direction("up")
+    if keys[pygame.K_LEFT]:
+    # g.move_direction("left")
+    if keys[pygame.K_DOWN]:
+    # g.move_direction("down")
+
+
+
     # --- Main event loop
-    for event in pygame.event.get():  # User did something
+    for event in pygame.event.get(): # User did something
+        clock = pygame.time.Clock()
+        clock.tick(60)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            start_screen=False
+
+
+
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
-    screen.fill((0, 0, 0))
-    screen.blit(display_welcome, (200, 100))
-    pygame.display.update()
+
+    if start_screen:
+        screen.blit(display_welcome, (200, 80))
+        screen.blit(display_welcome_1, (200, 120))
+        screen.blit(display_welcome_2, (200, 160))
+        screen.fill((0, 0, 0))
+    else:
+        pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
-
 
